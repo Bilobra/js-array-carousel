d9f498a5f1ee4ed9fbc688315b24c5ca50bae508
Bilobra <bra.carrozzo@gmail.com>
2022-07-26 14:43:43 +0200

    estrapolato .slides-wrapper

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 // console di prova
 console.log('prova')
+
+// generare contenuto Slider
+// array contenente i valori variabili dei contenuti dello slider
+const slides = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+]
+
+// estrapolare wrapper dal dom
+const slidesWrapperElement = document.querySelector('.slides-wrapper')
+slidesWrapperElement.innerHTML = ''
+console.dir(slidesWrapperElement)
